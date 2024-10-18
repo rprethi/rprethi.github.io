3257e548a0cc9428739a8bc8bfbb7da1485bffc8
rprethi <2271821@cmontmorency.qc.ca>
2024-10-18 14:51:01 -0400

    encore des corrections!

--- scriptmain.js
+++ scriptmain.js
@@ -2,7 +2,7 @@
 
 var typed = new Typed(".auto-typed", {
   strings: ["le développement web", "le design web", "la programmation web"],
-  typeSpeed: 150,
-  backSpeed: 150,
+  typeSpeed: 70,
+  backSpeed: 70,
   loop: true,
 });
